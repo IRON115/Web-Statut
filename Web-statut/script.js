681ce7f860e308fa16bedbe2b7137cb096fb5121
Kakao <98341086+IRON115@users.noreply.github.com>
2023-01-27 09:21:04 +0300

    Add files via upload

---
+++ Web-statut/script.js
@@ -0,0 +1,29 @@
+ $(document).ready(function ()
+ {$('.slider').slick();});
+
+  $(document).ready(function slider ()
+  {
+	$('.second_slider').slick( 
+		{
+			slidesToShow: 3, slidesToScroll: 5, infinite: true, prevArrow: '.first_backward_button', nextArrow: '.first_forward_button',
+		});
+	});
+  $(document).ready(function ()
+  {
+	$('.third_slider').slick( 
+		{
+			slidesToShow: 3, slidesToScroll: 5, infinite: true, prevArrow: '.second_backward_button', nextArrow: '.second_forward_button',
+		});
+	});
+	$(document).ready(function ()
+	{
+		$('.fourth_slider').slick( 
+			{
+				slidesToShow: 3, slidesToScroll: 5, infinite: true, prevArrow: '.third_backward_button', nextArrow: '.third_forward_button',
+			});
+		});
+		let openingMenu = document.querySelector('.opening_catalog_block');
+		function hide () 
+		{openingMenu.classList.toggle('hide_catalog_block');}
+ 
+
